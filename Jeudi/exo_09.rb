f53429f9ba77903401bbb48ccf7ee29358d0a73e
badr96 <badreddine.machmachi@efrei.net>
2018-06-09 20:04:39 +0200

    programme asking for last and first name

---
+++ Jeudi/exo_09.rb
@@ -0,0 +1,7 @@
+puts"hello dear what's your first name: "
+f_name = gets.chomp
+
+puts"\nand what is your last name: "
+l_name = gets.chomp
+
+puts"\nhello Mr.#{f_name} #{l_name} it's a pleasure to have you"
