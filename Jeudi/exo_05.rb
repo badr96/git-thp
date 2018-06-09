24d6067ad3cc22f0ee29785c1b80cbaf2bde964e
badr96 <badreddine.machmachi@efrei.net>
2018-06-09 19:11:22 +0200

    each line is commented && the #{} serve for the calculating inside the function puts

---
+++ Jeudi/exo_05.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heures de travail à THP" #this one is one sayingthe purpose of the programme
+puts "Travail : #{10 * 5 * 11}"							 #do the calculation inside the bracket
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"			 # give the time of working in minutes
+
+puts "Et en secondes ?"									 #a string 
+
+puts 10 * 5 * 11 * 60 * 60								 #it give time of working in secondes
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"		 #a string 
+		
+puts 3 + 2 < 5 - 7										 #return that the is false 
+	
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"					 #calculate 3+2
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"					 #calculate 5-7
+
+puts "Ok, c'est faux alors !"							 # a string who confirm it
+
+puts "C'est drôle ça, faisons-en plus :"				 # on s'en jaye 
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"	#return the true cuz 5 is bigger than -2
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"# return true because 5 is bigger or equals to -2
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"# return false because u know why haha 
