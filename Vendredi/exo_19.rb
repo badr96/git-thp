637e9ef7b3e521be4dc8c2033629e87ef13a858d
badr96 <badreddine.machmachi@efrei.net>
2018-06-16 19:10:42 +0200

    right emails

---
+++ Vendredi/exo_19.rb
@@ -0,0 +1,13 @@
+number = 0
+i = 0
+
+while i != 50
+	i = i +1;
+	number = number + 1
+	new_emails = ["jean.dupont", number,"@email.fr"]
+	if number %2 == 0
+		puts "#{new_emails}"	
+	end
+	
+
+end	
