150af1f7f8cd7699609a9f91a23e333e2740a9b1
badr96 <badreddine.machmachi@efrei.net>
2018-06-16 19:05:18 +0200

    false emails

---
+++ Vendredi/exo_15.rb
@@ -0,0 +1,14 @@
+puts"hello buddy what was ur birt year: "
+birth_year = gets.to_i
+actual_year = 2017
+age = actual_year - birth_year
+
+
+puts"\nyear that past: "
+while actual_year != birth_year 
+	puts"#{actual_year}: #{age} "
+	
+	actual_year = actual_year - 1
+	age = actual_year - birth_year
+end
+
---
+++ Vendredi/exo_18.rb
@@ -0,0 +1,10 @@
+number = 0
+i = 0
+
+while i != 50
+	i = i +1;
+	number = number + 1
+	new_emails = ["jean.dupont", number,"@email.fr"]
+	puts "#{new_emails}"
+
+end	
